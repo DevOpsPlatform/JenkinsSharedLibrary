6e0533870b70a591519a87a2e86b0e6ee0bb9369
venkatasykam <venkatasykam@yahoo.com>
2020-04-17 12:18:54 +0000

    Update buildmyapp.groovy

--- vars/buildmyapp.groovy
+++ vars/buildmyapp.groovy
@@ -1,17 +1,12 @@
-#!/usr/bin/env groovy
 
-def call(body){
+def call(pipelineParams){
+
     
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-    
-    def buidVersion = config.get("buidVersion")
+    def buidVersion = $pipelineParams.buidVersion
 
     node{
       stage("sample"){
-          println "sample stage from shared library ${config}"
+          println "sample stage from shared library ${pipelineParams.buidVersion}"
           
           println "buidVersion: ${buidVersion}"
       }
