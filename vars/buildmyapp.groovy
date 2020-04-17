e870b175bb53e4d3353c26f72062e5c8d0e2de1d
venkatasykam <venkatasykam@yahoo.com>
2020-04-17 12:02:34 +0000

    Update buildmyapp.groovy

--- vars/buildmyapp.groovy
+++ vars/buildmyapp.groovy
@@ -1,9 +1,19 @@
+#!/usr/bin/env groovy
 
 def call(body){
+    
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    
+    def buidVersion = config.get("buidVersion")
 
     node{
       stage("sample"){
-        println "sample stage from shared library"
+          println "sample stage from shared library ${config}"
+          
+          println "buidVersion: ${buidVersion}"
       }
     }
 
